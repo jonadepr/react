87479caa7fdf77d7dbc330bd3f3c2252ba039dbc
Alfonso Tienda <atienda@iprocuratio.com>
2020-04-08 09:48:00 +0200

    ejercicios estado

---
+++ 12-ejercicios-estado/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListaComentarios from './components/ListaComentarios'
+
+
+function App() {
+  return (
+    <div className="App">
+      <ListaComentarios></ListaComentarios>
+    </div>
+  );
+}
+
+export default App;
