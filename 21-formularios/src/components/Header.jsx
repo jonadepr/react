9d00a024ed47857d6a9758e23af3b17e48db8cc8
jonadepr <eljosele4@gmail.com>
2020-04-29 13:03:48 +0200

    terminado

--- 21-formularios/src/components/Header.jsx
+++ 21-formularios/src/components/Header.jsx
@@ -1,4 +1,12 @@
 import React, { Component } from 'react'
+import { BRAND } from '../config/config'
+
+import {
+
+    Link,
+
+} from 'react-router-dom'
+
 
 export class Header extends Component {
 
@@ -6,6 +14,14 @@ export class Header extends Component {
     render() {
         return (
             <div>
+                <h3>{BRAND}</h3>
+                
+                    <div>
+                        <Link to="/projects">Projects</Link>
+                    </div>
+                    <div>
+                        <Link to="/clientes">Clientes</Link>
+                    </div>
                 <h1 className="ui header">{this.props.texto}</h1>
             </div>
         )
