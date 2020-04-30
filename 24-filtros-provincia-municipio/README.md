a3fd68ef2cd2b2733711c98e5322c243a477ee58
jonadepr <eljosele4@gmail.com>
2020-04-30 12:13:14 +0200

    completado municipios y provincias

--- 24-filtros-provincia-municipio/README.md
+++ 24-filtros-provincia-municipio/README.md
@@ -1,5 +1,15 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+
+
+
+## Levantar el json server
+ json-server --watch db.json --port 4000
+ 
+
+
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- 24-filtros-provincia-municipio/src/FiltroProvincias.jsx
+++ 24-filtros-provincia-municipio/src/FiltroProvincias.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class FiltroProvincias extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            provincias: [],
+            provincia: "-1",
+            municipios: []
+        }
+    }
 
 
     componentDidMount() {
         // axios
         // me tengo que traer las provincias
-    }
+        axios.get(`http://localhost:4000/provincias`).then(
+                res =>{
+                    this.setState({
+                        provincias: res.data
+                    })
+                    console.log("provincias", this.state.provincias)
+                }
+            ).catch(
+                console.log
+            )
+        }
+    
 
-    onProvinciasChange () {
+    onProvinciasChange = (e) => {
         // cuando cambie la provincia:
         // axios tiene que ir a por los municios de esa provincia
-
+        console.log(e.target.value);//id de la provincia
+        const id = e.target.value;
+        // http://localhost:4000/municipios?provincia=1
+        axios.get(`http://localhost:4000/municipios?provincia=${id}`).then(
+            res =>{
+                this.setState({
+                    provincia: id,
+                    municipios: res.data
+                })
+                console.log("municipios", this.state.municipios)
+            }
+        ).catch(
+            console.log
+        )
 
     }
 
@@ -22,14 +56,18 @@ export class FiltroProvincias extends Component {
             <div>
               <div>
                Provincias:
-               <select onChange={console.log}>
-                   <option value={1}>Valencia</option>
+               <select onChange={this.onProvinciasChange} value={this.state.provincia}>
+                   {this.state.provincias.map(
+                       e => 
+                   <option value={e.id}>{e.nombre}</option>)}
                </select>
                </div> 
                <div>
                Municipios:
-               <select>
-                   <option value={1}>Segorbe</option>
+               <select disabled={this.state.provincia<0?true:false}>
+                   {this.state.municipios.map(
+                       e => 
+                   <option value={e.id}>{e.nombre}</option>)}
                </select>
                </div>
             </div>
