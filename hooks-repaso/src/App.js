836d3110e4d66fa71c6ca361b7d53cf095d07101
jonadepr <eljosele4@gmail.com>
2020-05-12 20:09:24 +0200

    repaso de hooks

---
+++ hooks-repaso/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Example from './Example'
+
+function App() {
+  return (
+    <div className="App">
+      <Example></Example>
+    </div>
+  );
+}
+
+export default App;
